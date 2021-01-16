c89446ec8f6d49e4ce8c8ca0009480a9295616f5
DanFouS <rouzmfawer@gmail.comgit config --global user.email rouzmfawer@gmail.comgit config --global user.email rouzmfawer@gmail.com>
2021-01-16 21:01:56 +0100

    azijeakenakzjebzajeb

--- Frontend/client/src/app/app-routing.module.ts
+++ Frontend/client/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NannyProfileComponent } from './nanny-profile/nanny-profile.component';
 import { NannyFormComponent } from './nanny-form/nanny-form.component';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
