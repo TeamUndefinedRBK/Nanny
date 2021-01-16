23069b477e241956bbca794467e022257b9cfd8b
DanFouS <rouzmfawer@gmail.comgit config --global user.email rouzmfawer@gmail.comgit config --global user.email rouzmfawer@gmail.com>
2021-01-16 12:13:05 +0100

    profile edit button

--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,46 @@
-const express = require ('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 //var mysql = require('mysql');
 //require("dotenv").config();
-const {db}=require('./database-mysql/indexdb')
-authRoutes=require ('./routes/userRouter');
+const path = require("path");
+const { db } = require("./database-mysql/indexdb");
+authRoutes = require("./routes/userRouter");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// app.set("views", path.join(__dirname));
+// app.set("view engine", "ejs");
+
+// app.get("/", function (req, res) {
+
+//   res.render("SampleForm", {
+//     nanny: nanny,
+//   });
+// });
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS'
-    );
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Accept, X-Custom-Header, Authorization'
-    );
-   next();
-    })
-  //setup connection to database
-  db.authenticate()
-  .then(() => console.log('Connection has been established successfully.'))
-  .catch(err => console.log('Unable to connect to the database:' + err))
-  app.use('/', authRoutes)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Accept, X-Custom-Header, Authorization"
+  );
+  next();
+});
+//setup connection to database
+db.authenticate()
+  .then(() => console.log("Connection has been established successfully."))
+  .catch((err) => console.log("Unable to connect to the database:" + err));
+app.use("/", authRoutes);
 
-
-
-app.listen(PORT, () => console.log (`listening on port ${PORT} and To get started, visit: http://localhost:5000`))
+app.listen(PORT, () =>
+  console.log(
+    `listening on port ${PORT} and To get started, visit: http://localhost:5000`
+  )
+);
--- Frontend/client/src/app/nanny-profile/nanny-profile.component.ts
+++ Frontend/client/src/app/nanny-profile/nanny-profile.component.ts
@@ -5,8 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nanny-profile.component.html',
   styleUrls: ['./nanny-profile.component.css'],
 })
-export class NannyProfileComponent implements OnInit {
-  constructor() {}
+export class NannyProfileComponent {
+  // constructor() {}
+  isReadOnly = true;
+  nanny = {
+    FirstName: '3li',
+    LastName: '3loulou',
+    Age: 59,
+    Email: '3li.3loulou@3alela.3lol',
+    Region: 'sidi 3li',
+    PhoneNumber: 333333333,
+    About: 'aboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutabout',
+  };
 
-  ngOnInit(): void {}
+  readAndChange() {
+    this.isReadOnly = !this.isReadOnly;
+    console.log(this.isReadOnly);
+  }
 }
